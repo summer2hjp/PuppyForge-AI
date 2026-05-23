93d1fbab2680a9d1920c03298f474f7e36d168e4
JiaPing Huang <1132176980@qq.com>
2026-05-23 22:11:32 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'via.placeholder.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.puppyforge.ai',
+      },
+    ],
   },
   experimental: {
     serverActions: {
