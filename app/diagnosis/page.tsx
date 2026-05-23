2394785138ba171312d17ed948323bfd37c2f5d3
JiaPing Huang <1132176980@qq.com>
2026-05-23 09:22:48 +0800

    创建诊断专页

---
+++ app/diagnosis/page.tsx
@@ -0,0 +1,11 @@
+import DiagnosisModule from '../../components/DiagnosisModule';
+
+export default function DiagnosisPage() {
+  return (
+    <div className="min-h-screen bg-zinc-950 text-white p-8">
+      <div className="max-w-4xl mx-auto">
+        <DiagnosisModule />
+      </div>
+    </div>
+  );
+}
