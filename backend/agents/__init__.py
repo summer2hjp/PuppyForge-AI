1355083e789ed0b90c8d0ce4ce0ec3c0eda06469
JiaPing Huang <1132176980@qq.com>
2026-05-30 06:40:59 +0000

    Add VisionAgent and MemoryAgent to agents module

--- backend/agents/__init__.py
+++ backend/agents/__init__.py
@@ -5,6 +5,8 @@ from .rebel_agent import RebelAgent
 from .diagnosis_agent import DiagnosisAgent
 from .growth_agent import GrowthAgent
 from .prediction_agent import PredictionAgent
+from .vision_agent import VisionAgent
+from .memory_agent import MemoryAgent
 
 # 单例实例
 trait_drift_agent = TraitDriftAgent()
@@ -13,13 +15,25 @@ rebel_agent = RebelAgent()
 diagnosis_agent = DiagnosisAgent()
 growth_agent = GrowthAgent()
 prediction_agent = PredictionAgent()
+vision_agent = VisionAgent()
+memory_agent_instance = MemoryAgent()
 
 __all__ = [
     "BaseAgent",
+    "TraitDriftAgent",
+    "MemoryWeaver",
+    "RebelAgent",
+    "DiagnosisAgent",
+    "GrowthAgent",
+    "PredictionAgent",
+    "VisionAgent",
+    "MemoryAgent",
     "trait_drift_agent",
     "memory_weaver",
     "rebel_agent",
     "diagnosis_agent",
     "growth_agent",
     "prediction_agent",
+    "vision_agent",
+    "memory_agent_instance",
 ]
